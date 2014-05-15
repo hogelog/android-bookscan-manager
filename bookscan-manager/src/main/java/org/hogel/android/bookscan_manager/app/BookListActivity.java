8d069fe58fd1219d084936631a13894031a1272c
hogelog <konbu.komuro@gmail.com>
2014-05-15 21:44:06 +0900

    Disable twopane

--- bookscan-manager/src/main/java/org/hogel/android/bookscan_manager/app/BookListActivity.java
+++ bookscan-manager/src/main/java/org/hogel/android/bookscan_manager/app/BookListActivity.java
@@ -25,32 +25,10 @@ import android.support.v4.app.FragmentActivity;
 public class BookListActivity extends FragmentActivity
         implements BookListFragment.Callbacks {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
-
-        if (findViewById(R.id.book_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-large and
-            // res/values-sw600dp). If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-
-            // In two-pane mode, list items should be given the
-            // 'activated' state when touched.
-            ((BookListFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.book_list))
-                    .setActivateOnItemClick(true);
-        }
-
-        // TODO: If exposing deep links into your app, handle intents here.
     }
 
     /**
@@ -59,24 +37,8 @@ public class BookListActivity extends FragmentActivity
      */
     @Override
     public void onItemSelected(String id) {
-        if (mTwoPane) {
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            Bundle arguments = new Bundle();
-            arguments.putString(BookDetailFragment.ARG_ITEM_ID, id);
-            BookDetailFragment fragment = new BookDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.book_detail_container, fragment)
-                    .commit();
-
-        } else {
-            // In single-pane mode, simply start the detail activity
-            // for the selected item ID.
-            Intent detailIntent = new Intent(this, BookDetailActivity.class);
-            detailIntent.putExtra(BookDetailFragment.ARG_ITEM_ID, id);
-            startActivity(detailIntent);
-        }
+        Intent detailIntent = new Intent(this, BookDetailActivity.class);
+        detailIntent.putExtra(BookDetailFragment.ARG_ITEM_ID, id);
+        startActivity(detailIntent);
     }
 }
