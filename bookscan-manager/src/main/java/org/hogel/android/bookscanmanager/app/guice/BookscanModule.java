2f8bcf315823b0509cde3d70c10929d3f0c9f396
hogelog <konbu.komuro@gmail.com>
2014-07-06 23:17:17 +0900

    Still login

--- bookscan-manager/src/main/java/org/hogel/android/bookscanmanager/app/guice/BookscanModule.java
+++ bookscan-manager/src/main/java/org/hogel/android/bookscanmanager/app/guice/BookscanModule.java
@@ -5,6 +5,8 @@ import android.app.Service;
 import android.content.Context;
 import com.google.inject.Binder;
 import com.google.inject.Module;
+import com.google.inject.Singleton;
+import org.hogel.android.bookscanmanager.app.util.Preferences;
 import org.hogel.bookscan.AsyncBookscanClient;
 import roboguice.inject.ContextSingleton;
 
@@ -14,11 +16,10 @@ import javax.inject.Provider;
 public class BookscanModule implements Module {
     @Override
     public void configure(Binder binder) {
-        binder.bind(DownloadManager.class).toProvider(DownloadManagerProvider.class);
-        binder.bind(AsyncBookscanClient.class).toInstance(new AsyncBookscanClient());
+        binder.bind(DownloadManager.class).toProvider(DownloadManagerProvider.class).in(ContextSingleton.class);
+        binder.bind(AsyncBookscanClient.class).toProvider(AsyncBookscanClientProvider.class).in(Singleton.class);
     }
 
-    @ContextSingleton
     public static class DownloadManagerProvider implements Provider<DownloadManager> {
         @Inject
         private Context context;
@@ -26,6 +27,18 @@ public class BookscanModule implements Module {
         @Override
         public DownloadManager get() {
             return (DownloadManager) context.getSystemService(Service.DOWNLOAD_SERVICE);
+        }
+    }
+
+    public static class AsyncBookscanClientProvider implements Provider<AsyncBookscanClient> {
+        @Inject
+        private Preferences preferences;
+
+        @Override
+        public AsyncBookscanClient get() {
+            final AsyncBookscanClient client = new AsyncBookscanClient();
+            client.putCookies(preferences.getCookies());
+            return client;
         }
     }
 }
